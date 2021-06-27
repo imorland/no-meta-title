e1559563f0083b1e2ece56f44b550b1c4767a303
Ian Morland <ian@morland.me>
2021-06-27 22:56:06 +0100

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,7 +2,7 @@ import app from 'flarum/forum/app';
 import { override } from 'flarum/common/extend';
 import Application from 'flarum/common/Application';
 
-app.initializers.add('fof/ban-ips', () => {
+app.initializers.add('ianm-no-meta-title', () => {
     override(Application.prototype, 'updateTitle', function (original) {
         const count = this.titleCount ? `(${this.titleCount}) ` : '';
         const pageTitle = this.title && m.route.get() !== this.forum.attribute('basePath') + '/' ? this.title : '';
