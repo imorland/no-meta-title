413ac8e10dc34a2b948d7a35d385f3bb76c4c728
Ian Morland <ian@morland.me>
2022-06-08 22:26:00 +0100

    Webpack 5

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,12 +3,12 @@ import { override } from 'flarum/common/extend';
 import Application from 'flarum/common/Application';
 
 app.initializers.add('ianm-no-meta-title', () => {
-    override(Application.prototype, 'updateTitle', function (original) {
-        const count = this.titleCount ? `(${this.titleCount}) ` : '';
-        const pageTitle = this.title && m.route.get() !== this.forum.attribute('basePath') + '/' ? this.title : '';
-        const title = this.forum.attribute('title');
+  override(Application.prototype, 'updateTitle', function (original) {
+    const count = this.titleCount ? `(${this.titleCount}) ` : '';
+    const pageTitle = this.title && m.route.get() !== this.forum.attribute('basePath') + '/' ? this.title : '';
+    const title = this.forum.attribute('title');
 
-        const titleForDisplay = pageTitle ? pageTitle : title;
-        document.title = count + titleForDisplay;
-    });
+    const titleForDisplay = pageTitle ? pageTitle : title;
+    document.title = count + titleForDisplay;
+  });
 });
